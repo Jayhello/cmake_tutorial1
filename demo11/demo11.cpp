7bf9664a876a527e46730a4d219a85e1141f4ebb
Jayhello <haluoluo@qq.com>
2018-12-03 15:50:28 +0800

    1. my cmake tutorial

--- demo11/demo11.cpp
+++ demo11/demo11.cpp
@@ -5,8 +5,12 @@
 #include <stdio.h>
 #include "demoConfig.h"
 
+#include "demo7/demo7_1.h"
+
 int main(int argc, char* argv[]){
 
     printf("%s version: %d.%d \n", argv[0], demo11_VERSION_MAJOR, demo11_VERSION_MINOR);
+
+    demo7::print_demo71();
 }
 
